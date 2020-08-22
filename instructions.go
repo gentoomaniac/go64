217a3fc8c948bacdb4583c1c4b75e420663192e0
Marco Siebecke <marco@siebecke.se>
2020-08-23 00:45:44 +0200

    that should never have been commited :)

--- instructions.go
+++ instructions.go
@@ -1,12 +1,1 @@
 package main
-
-        // A | addr
-        func ORA( addr uint16)
-        {
-            cycleLock.enterCycle();
-            A |= getByteFromMemory(address, lockToCycle: false);
-
-            setProcessorStatusBit(ProcessorStatus.Z, isSet: (A == 0));
-            setProcessorStatusBit(ProcessorStatus.N, isSet: ((A & (byte)ProcessorStatus.N) != 0));
-            cycleLock.exitCycle();
-        }
