930f8c340406845926a6d31dd198eabacb3b8ab2
Marco Siebecke <marco@siebecke.se>
2020-08-23 00:55:19 +0200

    added tests for PCL and PCH

--- tests/mos6510_test.go
+++ tests/mos6510_test.go
@@ -36,5 +36,54 @@ func TestMOS6510(t *testing.T) {
 				g.Assert(mos6510.PC()).Equal(value)
 			}
 		})
+
+		g.It("PCL", func() {
+			var blankMemory [0x10000]byte
+			mos6510 := &mpu.MOS6510{}
+			mos6510.Memory = &blankMemory
+
+			value := uint8(0x00)
+			mos6510.SetPCL(value)
+			g.Assert(mos6510.PCL()).Equal(value)
+			g.Assert(mos6510.PC()).Equal(uint16(value))
+
+			value = uint8(0xff)
+			mos6510.SetPCL(value)
+			g.Assert(mos6510.PCL()).Equal(value)
+			g.Assert(mos6510.PC()).Equal(uint16(value))
+
+			for i := 0; i < RandomTestCount; i++ {
+				value = uint8(rand.Intn(0xff))
+				mos6510.SetPCL(value)
+				g.Assert(mos6510.PCL()).Equal(value)
+				g.Assert(mos6510.PC()).Equal(uint16(value))
+			}
+		})
+
+		g.It("PCH", func() {
+			var blankMemory [0x10000]byte
+			mos6510 := &mpu.MOS6510{}
+			mos6510.Memory = &blankMemory
+
+			value := uint8(0x00)
+			pcValue := uint16(value) << 8
+			mos6510.SetPCH(value)
+			g.Assert(mos6510.PCH()).Equal(value)
+			g.Assert(mos6510.PC()).Equal(uint16(pcValue))
+
+			value = uint8(0xff)
+			pcValue = uint16(value) << 8
+			mos6510.SetPCH(value)
+			g.Assert(mos6510.PCH()).Equal(value)
+			g.Assert(mos6510.PC()).Equal(uint16(pcValue))
+
+			for i := 0; i < RandomTestCount; i++ {
+				value = uint8(rand.Intn(0xff))
+				pcValue = uint16(value) << 8
+				mos6510.SetPCH(value)
+				g.Assert(mos6510.PCH()).Equal(value)
+				g.Assert(mos6510.PC()).Equal(uint16(pcValue))
+			}
+		})
 	})
 }
